dee298ee87c0f72e45a4600c3de180d6cb2c66d8
Stille <stille@ioiox.com>
2021-09-07 15:21:32 +0800

    Optimize acme to test

--- acme/acme.sh
+++ acme/acme.sh
@@ -10,28 +10,35 @@ RedBG="\033[41;37m"
 Font="\033[0m"
 # fonts color
 
-
 DOMAIN=$(cat /conf/account.conf | awk -F= '{if($1~"DOMAIN")print $2}')
 DNSAPI=$(cat /conf/account.conf | awk -F= '{if($1~"DNSAPI")print $2}')
 
-acme (){
-    cat /conf/account.conf > /acme.sh/account.conf
+acme() {
+    cat /conf/account.conf >/acme.sh/account.conf
     /root/.acme.sh/acme.sh --upgrade
     /root/.acme.sh/acme.sh --register-account -m your@domain.com --server zerossl
     /root/.acme.sh/acme.sh --issue $* --dns ${DNSAPI} -d ${DOMAIN} -d \*.${DOMAIN}
-    cp /acme.sh/${DOMAIN}/fullchain.cer /ssl/${DOMAIN}.cer
-    cp /acme.sh/${DOMAIN}/${DOMAIN}.key /ssl/${DOMAIN}.key
-    mv /acme.sh/${DOMAIN} /acme.sh/${DOMAIN}-$(date +%Y)-$(date +%m)-$(date +%d)-${RANDOM}
     rm -rf /acme.sh/ca
     rm -rf /acme.sh/http.header
+    if [ -f /acme.sh/${DOMAIN}/fullchain.cer ] && [ -f /acme.sh/${DOMAIN}/${DOMAIN}.key ]; then
+        rm -rf /ssl/${DOMAIN}.cer
+        rm -rf /ssl/${DOMAIN}.key
+        cp /acme.sh/${DOMAIN}/fullchain.cer /ssl/${DOMAIN}.cer
+        cp /acme.sh/${DOMAIN}/${DOMAIN}.key /ssl/${DOMAIN}.key
+        mv /acme.sh/${DOMAIN} /acme.sh/${DOMAIN}-$(date +%Y)-$(date +%m)-$(date +%d)-${RANDOM}
+    else
+        echo "证书申请失败,已退出脚本,请重新尝试."
+        rm -rf /acme.sh/${DOMAIN}
+        exit 1
+    fi
 }
 
-if [ ! -n "${DOMAIN}" ] ; then
+if [ ! -n "${DOMAIN}" ]; then
     echo "请检查 account.conf 信息是否正确"
 else
     acme $*
 fi
 
-cat >/var/spool/cron/crontabs/root<<EOF
-0 0 3 * * /conf/acme.sh >/dev/null 2>&1
+cat >/var/spool/cron/crontabs/root <<EOF
+0 0 10 * * /conf/acme.sh >/dev/null 2>&1
 EOF
